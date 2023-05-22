f15bb862be2e75e2dd145c2987249478d7331082
Денис Писарев <denis35@mail.ru>
2023-05-22 21:34:11 +0300

    Созданы 2 view функции create_product и orders_list

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,18 +1,15 @@
 from timeit import default_timer
 
 from django.contrib.auth.models import Group
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse, HttpRequest
 
+from .forms import ProductForm, OrderForm
 from shopapp.models import Product, Order
 
 
 def shop_index(request: HttpRequest):
-    products = [
-        ('Laptop', 1999),
-        ('Desktop', 2999),
-        ('Smartphone', 999)
-    ]
+    products = Product.objects.all()
     context = {
         'time_running': default_timer(),
         'products': products,
@@ -34,8 +31,41 @@ def products_list(request: HttpRequest):
     return render(request, 'shopapp/products-list.html', context=context)
 
 
+def create_product(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            url = reverse("shopapp:products_list")
+            print("Redirect URL:", url)
+            return redirect(url)
+        else:
+            print("Form is not valid:", form.errors)
+    else:
+        form = ProductForm()
+    context = {
+        "form": form,
+    }
+    return render(request, 'shopapp/create-product.html', context=context)
+
+
 def orders_list(request: HttpRequest):
     context = {
         'orders': Order.objects.select_related("user").prefetch_related('products').all(),
     }
     return render(request, 'shopapp/orders-list.html', context=context)
+
+
+def create_order(request):
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            order.user = request.user
+            selected_products = request.POST.getlist('products')
+            order.products.set(selected_products)
+            order.save()
+            return redirect('order_success')
+    else:
+        form = OrderForm()
+    return render(request, 'shopapp/create-order.html', {'form': form})
