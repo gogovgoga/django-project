ae825028f4e4d02a0ad6e4a339c3b646ef42897e
Денис Писарев <denis35@mail.ru>
2023-05-31 14:53:25 +0300

    Создан файл vies.py в приложении accounts

---
+++ mysite/accounts/views.py
@@ -0,0 +1,68 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LogoutView
+from django.http import HttpRequest, HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
+
+from accounts.models import Profile
+
+
+class AboutMeView(TemplateView):
+    template_name = "accounts/about-me.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        has_create_product_perm = user.has_perm('shopapp.create_product')
+        context['user'] = user
+        context['has_create_product_perm'] = has_create_product_perm
+        return context
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = "accounts/register.html"
+    success_url = reverse_lazy("accounts:about-me")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        Profile.objects.create(user=self.object)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password1")
+        user = authenticate(
+            self.request,
+            username=username,
+            password=password,
+        )
+        login(request=self.request, user=user)
+        return response
+
+
+class MyLogoutView(LogoutView):
+    next_page = reverse_lazy("accounts:login")
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def set_cookie_view(request: HttpRequest) -> HttpResponse:
+    response = HttpResponse("Cookie set")
+    response.set_cookie("fizz", "buzz", max_age=3600)
+    return response
+
+
+def get_cookie_view(request: HttpRequest) -> HttpResponse:
+    value = request.COOKIES.get("fizz", "default_value")
+    return HttpResponse(f"Cookie value: {value!r}")
+
+
+@permission_required("accounts.view_profile", raise_exception=True)
+def set_session_view(request: HttpRequest) -> HttpResponse:
+    request.session["foobar"] = "spameggs"
+    return HttpResponse("Session set!")
+
+
+@login_required
+def get_session_view(request: HttpRequest) -> HttpResponse:
+    value = request.session.get("foobar", "default")
+    return HttpResponse(f"Session value: {value!r}")
