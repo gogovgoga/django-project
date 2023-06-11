a303b7890f1162a5581f1c459e80b48c53963498
Денис Писарев <denis35@mail.ru>
2023-06-11 18:07:37 +0300

    Добавлено preview в Product, receipt в Order

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -2,11 +2,16 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+def product_preview_directory_path(instance: "Product", filename: str) -> str:
+    return "products/product_{pk}/preview/{filename}".format(
+        pk=instance.pk,
+        filename=filename,
+    )
+
+
 class Product(models.Model):
     class Meta:
         ordering = ['name', 'price']
-        # db_table = "tech_products"
-        # verbose_name_plural = "products"
 
     name = models.CharField(max_length=100)
     description = models.TextField(null=False, blank=True)
@@ -15,6 +20,7 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     archived = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True)
+    preview = models.ImageField(null=True, blank=True, upload_to="product_preview_directory_path")
 
     def __str__(self) -> str:
         return f"Product(pk={self.pk}, name={self.name!r})"
@@ -26,3 +32,4 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     products = models.ManyToManyField(Product, related_name="orders")
+    receipt = models.FileField(null=True, upload_to='orders/receipts/')
