e4dee4c8000eccd8179ec2880590b30efff4a905
Денис Писарев <denis35@mail.ru>
2023-06-12 18:44:59 +0300

    Изменен класс ProductForm, добавлена возможность загрузки сразу нескольких файлов

--- mysite/shopapp/forms.py
+++ mysite/shopapp/forms.py
@@ -1,13 +1,15 @@
 from django import forms
 from django.contrib.auth.models import Group
-
+from multiupload.fields import MultiFileField
 from shopapp.models import Product, Order
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = "name", "price", "description", "discount"
+        fields = "name", "price", "description", "discount", "preview", "images"
+
+    images = MultiFileField(min_num=1, max_num=10)
 
 
 class OrderForm(forms.ModelForm):
