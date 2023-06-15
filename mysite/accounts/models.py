80732c4bc106ae6ce1d834b06ed9e55320df0d95
Денис Писарев <denis35@mail.ru>
2023-06-15 17:14:46 +0300

    Добавлено поле avatar в класс Profile

--- mysite/accounts/models.py
+++ mysite/accounts/models.py
@@ -1,9 +1,27 @@
+import os
+
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+def avatar_upload_path(instance, filename):
+
+    ext = filename.split('.')[-1]
+    filename = f'{instance.user.username}_avatar.{ext}'
+
+    return os.path.join('avatars', filename)
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     agreement_accepted = models.BooleanField(default=False)
+    avatar = models.ImageField(upload_to=avatar_upload_path, null=True, blank=True)
 
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
