7d691134904de20ccdad1a8f59f0d5b422bcb123
Денис Писарев <denis35@mail.ru>
2023-06-19 18:45:08 +0300

    django-filter подключен в настройках, а также указан стандартный бэкенд для фильтрации DEFAULT_FILTER_BACKENDS

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_filters',
 
     'shopapp.apps.ShopappConfig',
     'requestdataapp.apps.RequestdataappConfig',
@@ -152,4 +153,7 @@ LOGIN_URL = reverse_lazy("accounts:login")
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+    ]
 }
